93f37780024a43637aabf3e64bbc0b6d7e8c18ea
1515012 <1515011@kiit.ac.in>
2017-07-26 14:33:06 +0530

    added widget directly

--- app/src/main/java/youtubeapidemo/examples/com/bakingapp/DetailIngredientFragment.java
+++ app/src/main/java/youtubeapidemo/examples/com/bakingapp/DetailIngredientFragment.java
@@ -1,11 +1,10 @@
 package youtubeapidemo.examples.com.bakingapp;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -14,6 +13,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -39,14 +39,15 @@ public class DetailIngredientFragment extends Fragment {
     RecyclerView recyclerView;
     @BindView(R.id.cook_custom_button)
     Button cookingButton;
-
+    @BindView(R.id.card_view_ingredients)
+    CardView cardViewIngredients;
 
     public static final String TAG = DetailIngredientFragment.class.getSimpleName();
     private IngredientAdapter ingredientAdapter;
     private int pos;
     private ArrayList<String> arrayList;
     private ArrayList<Description> descriptionArrayList;
-    private ProgressDialog progressDialog;
+    public static final String WIDGIT_POSITION = "WIDGIT POSITION";
 
     @Nullable
     @Override
@@ -54,13 +55,6 @@ public class DetailIngredientFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_detail_ingredient, container, false);
         ButterKnife.bind(this, root);
-        /*    progressDialog = new ProgressDialog(getActivity());
-            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progressDialog.setCancelable(false);
-            progressDialog.setIndeterminate(false);
-            progressDialog.setTitle(getString(R.string.progress_dialog));
-            progressDialog.setMessage(getString(R.string.please_wait));
-            progressDialog.show();*/
 
         Intent intent = getActivity().getIntent();
 
@@ -68,20 +62,25 @@ public class DetailIngredientFragment extends Fragment {
 
         descriptionArrayList = new ArrayList<>();
         if (intent != null) {
-            pos = intent.getIntExtra(getString(R.string.LIST_KEY), 0);
+            if ((intent.getAction()).equals(WIDGIT_POSITION)) {
+                pos = intent.getIntExtra("WIDGIT_POSITION_CLICKED", 0);
+            } else if (intent.getAction().equals("DETAILS")) {
+                pos = intent.getIntExtra(getString(R.string.LIST_KEY), 0);
+            }
         } else {
-
             pos = getArguments().getInt("position");
+            Log.e(TAG, ".....................pos=" + pos + "");
         }
+
         makeJsonArrayRequest();
+
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         ingredientAdapter = new IngredientAdapter(getActivity(), arrayList);
         recyclerView.setAdapter(ingredientAdapter);
-        if (getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            RecyclerView.OnItemTouchListener disabler = new RecyclerViewDisabler();
-            recyclerView.addOnItemTouchListener(disabler);
-        }
+        RecyclerView.OnItemTouchListener disabler = new RecyclerViewDisabler();
+        recyclerView.addOnItemTouchListener(disabler);
+
         //   if(getActivity().getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE)
         /*    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.FILL_PARENT);
 
@@ -89,16 +88,7 @@ public class DetailIngredientFragment extends Fragment {
 
         cooingButton.setLayoutParams(params);*/
 
-        cookingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), DescriptionActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putParcelableArrayList("list", descriptionArrayList);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
+
         return root;
     }
 
@@ -109,6 +99,7 @@ public class DetailIngredientFragment extends Fragment {
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
+                        Toast.makeText(getActivity(),"Hello",Toast.LENGTH_SHORT).show();
                         String ingredientsRequirent = null;
                         try {
                             JSONObject recipe = (JSONObject) response.get(pos);
@@ -116,8 +107,7 @@ public class DetailIngredientFragment extends Fragment {
                             for (int j = 0; j < ingredients.length(); j++) {
                                 JSONObject ingr = (JSONObject) ingredients.get(j);
                                 ingredientsRequirent = ingr.getString("ingredient") + " - " +
-                                        ingr.getInt("quantity") + " " + ingr.getString("measure");
-                                Log.i(TAG, ingredientsRequirent);
+                                        ingr.getDouble("quantity") + " " + ingr.getString("measure");
                                 arrayList.add(ingredientsRequirent);
                             }
                             JSONArray steps = recipe.getJSONArray("steps");
@@ -132,20 +122,30 @@ public class DetailIngredientFragment extends Fragment {
                             ingredientAdapter.changeData(arrayList);
                        /*      if (progressDialog.isShowing())
                                     progressDialog.dismiss();*/
-
+                            cookingButton.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Intent intent = new Intent(getActivity(), DescriptionActivity.class);
+                                    Bundle bundle = new Bundle();
+                                    bundle.putParcelableArrayList("list", descriptionArrayList);
+                                    intent.putExtras(bundle);
+                                    startActivity(intent);
+                                }
+                            });
+                            cookingButton.setVisibility(View.VISIBLE);
+                            cardViewIngredients.setVisibility(View.VISIBLE);
 
                         } catch (JSONException e) {
-
                             e.printStackTrace();
                             Log.i(TAG, "Error: " + e.getMessage());
+                        } finally {
+                            if (IngredientActivity.progressBar != null)
+                                IngredientActivity.progressBar.setVisibility(View.INVISIBLE);
                         }
-
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                if (progressDialog.isShowing())
-                    progressDialog.dismiss();
                 VolleyLog.d(TAG, "Error: " + error.getMessage());
             }
         });
